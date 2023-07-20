636af74bd6dd9f6301e9068ba98190f74df440b7
Paul Leblanc <azikx4qg@gmail.com>
2023-07-20 17:29:18 +0200

    Services section and nav bar corrections

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
 var navLinks = document.querySelectorAll("nav a");
+var isScrolling = false;
 
 navLinks.forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     navLinks.forEach((link) => link.classList.remove("nav-active"));
     this.classList.add("nav-active");
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+
+    isScrolling = true;
+    var target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+
+    setTimeout(() => {
+      isScrolling = false;
+    }, 1000);
   });
 });
 
 window.addEventListener("scroll", function () {
+  if (isScrolling) return;
+
   let fromTop = window.scrollY + window.innerHeight / 3;
 
   navLinks.forEach((link) => {
     let section = document.querySelector(link.hash);
 
-    if (
+    if (link.hash === "#Home") {
+      if (window.scrollY < 100) {
+        navLinks.forEach((link) => link.classList.remove("nav-active"));
+        link.classList.add("nav-active");
+      }
+    } else if (
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
     ) {
-      navLinks.forEach((link) => link.classList.remove("nav-active")); // Ajout de cette ligne
+      navLinks.forEach((link) => link.classList.remove("nav-active"));
       link.classList.add("nav-active");
     } else {
       link.classList.remove("nav-active");
